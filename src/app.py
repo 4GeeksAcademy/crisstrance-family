939ea26a4e31173848a7fbf768e7d09be4e6545d
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-09-20 19:28:26 +0000

    FEAT family members

--- src/app.py
+++ src/app.py
@@ -20,19 +20,46 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code  # generate sitemap with all your endpoints
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+@app.route('/members', methods=['GET', 'POST'])
+def members():
+    response_body = {}
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body["message"] = "Listado de miembros de la familia"
+        response_body["results"] = members
+        return jsonify(response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        jackson_family.add_member(data)
+        response_body["message"] = "Quiero agregar un member"
+        response_body["results"] = jackson_family.get_all_members()
+        return response_body, 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                   "family": members}
-    return jsonify(response_body), 200
+
+@app.route('/members/<int:member_id>', methods=['GET', 'PUT', 'DELETE'])
+def member(member_id):
+    response_body = {}
+    member = jackson_family.get_member(member_id)
+    if request.method == 'GET':
+        response_body["message"] = f"mensaje del GET del member id: {member_id}"
+        response_body["results"] = member[0]
+        return response_body, 200
+    if request.method == 'PUT':
+        pass
+        response_body["message"] = "mensaje del PUT"
+        response_body["results"] = {}
+        return response_body, 200
+    if request.method == 'DELETE':
+        jackson_family.delete_member(member_id)
+        response_body["message"] = f"mensaje del DELETE del member id: {member_id}"
+        response_body["results"] = jackson_family.get_all_members()
+        return response_body, 200
 
 
 # this only runs if `$ python src/app.py` is executed
